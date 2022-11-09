df50f4cffc3e114dd2c264c1f13b5cb337843753
ShinSeongHwan <kory199@Gmail.com>
2022-11-09 09:46:30 +0900

    UI
    Fixed Pointer

--- Assets/UI/Script/PointerEvent.cs
+++ Assets/UI/Script/PointerEvent.cs
@@ -4,27 +4,44 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PointerEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+public class PointerEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     Image[] _backgroundImage;
-
-    private void Start()
-    {
-        
-    }
 
     private void Awake()
     {
         _backgroundImage = GetComponentsInChildren<Image>();
     }
 
+    private void Start()
+    {
+        // get Children image
+        _backgroundImage[1].fillAmount = 0f;
+        _backgroundImage[1].enabled = false;
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         _backgroundImage[1].enabled = true;
+        StartCoroutine(ChangeFilled());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        // fillamount reset
+        _backgroundImage[1].fillAmount = 0f;
         _backgroundImage[1].enabled = false;
+    }
+
+    // change Image FillAmout to 0~1
+    IEnumerator ChangeFilled()
+    {
+        float fillSpeed = 0.05f;
+
+        while (_backgroundImage[1].fillAmount < 1)
+        {
+            yield return new WaitForSeconds(0.01f);
+            _backgroundImage[1].fillAmount += fillSpeed;
+        }
+        yield return null;
     }
 }

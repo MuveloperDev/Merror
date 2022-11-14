25d1a0de819cae2cb31edc92af18b9fc2b82bbc1
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2022-11-14 19:39:59 +0900

    Fix AudioManager and Chap1 Scene

--- Assets/Public/AudioManager.cs
+++ Assets/Public/AudioManager.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public enum Type { Player, Identity, HorrorEvnets};
+    public enum Type { Player, Identity, HorrorEvnets, Interactable, Environment};
     private AudioManager() { }
     [SerializeField] ScriptableObj PlayerObj = null;
-    [SerializeField] ScriptableObj PropsObj = null;
+    [SerializeField] ScriptableObj InteractableObj = null;
     [SerializeField] ScriptableObj EnviromentObj = null;
     [SerializeField] ScriptableObj IdentityObj = null;
     [SerializeField] ScriptableObj HorrorEvnetsObj = null;
 
     private Dictionary<string, AudioClip> PlayerClips = new Dictionary<string, AudioClip>();
-    private Dictionary<string, AudioClip> PropsClips = new Dictionary<string, AudioClip>();
+    private Dictionary<string, AudioClip> InteractableClips = new Dictionary<string, AudioClip>();
     private Dictionary<string, AudioClip> EnviromentClips = new Dictionary<string, AudioClip>();
     private Dictionary<string, AudioClip> IdentiyuClips = new Dictionary<string, AudioClip>();
     private Dictionary<string, AudioClip> HorrorEvnetsClips = new Dictionary<string, AudioClip>();
@@ -20,12 +20,10 @@ public class AudioManager : MonoBehaviour
     private void Awake()
     {
         AddAllClipsToDic(PlayerObj);
-        AddAllClipsToDic(PropsObj);
+        AddAllClipsToDic(InteractableObj);
         AddAllClipsToDic(EnviromentObj);
         AddAllClipsToDic(IdentityObj);
         AddAllClipsToDic(HorrorEvnetsObj);
-
-        Debug.Log(IdentiyuClips.Count);
     }
 
     /// <summary>
@@ -36,21 +34,21 @@ public class AudioManager : MonoBehaviour
     {
         switch (obj.name)
         {
-            case "PlayerSounds":
+            case "Player":
                 for (int i = 0; i < obj.Sounds.Length; i++)
                 {
                     PlayerClips.Add(PlayerObj.Sounds[i].name, PlayerObj.Sounds[i]);
                 }
                 break;
 
-            case "PropsSounds":
+            case "Interactable":
                 for (int i = 0; i < obj.Sounds.Length; i++)
                 {
-                    PropsClips.Add(PropsObj.Sounds[i].name, PropsObj.Sounds[i]);
+                    InteractableClips.Add(InteractableObj.Sounds[i].name, InteractableObj.Sounds[i]);
                 }
                 break;
 
-            case "EnvironmentSounds":
+            case "Environment":
                 for (int i = 0; i < obj.Sounds.Length; i++)
                 {
                     EnviromentClips.Add(EnviromentObj.Sounds[i].name, EnviromentObj.Sounds[i]);
@@ -73,17 +71,6 @@ public class AudioManager : MonoBehaviour
     }
 
     /// <summary>
-    /// return requested Audioclip for interactableObj
-    /// </summary>
-    /// <param name="soundType"> The type containing the audio clip to request. </param>
-    /// <param name="clipName"> The name of requested audioclip </param>
-    /// <returns></returns>
-    public AudioClip GetClip(Interactable.SoundType soundType, string clipName)
-    {
-        return CheckType(soundType).ContainsKey(clipName) ? CheckType(soundType)[clipName] : null;
-    }
-
-    /// <summary>
     /// return requested Audioclip for others
     /// </summary>
     /// <param name="type">The type in AudioManager containing the audio clip to request.</param>
@@ -92,41 +79,6 @@ public class AudioManager : MonoBehaviour
     public AudioClip GetClip(Type type, string clipName)
     {
         return CheckType(type).ContainsKey(clipName) ? CheckType(type)[clipName] : null;
-    }
-
-    /// <summary>
-    /// Return the corresponding Dictionary via type
-    /// </summary>
-    /// <param name="soundType"> The type containing the audio clip to request. </param>
-    /// <returns></returns>
-    private Dictionary<string, AudioClip> CheckType(Interactable.SoundType soundType)
-    {
-        Dictionary<string, AudioClip> curdic = new Dictionary<string, AudioClip>();
-        switch (soundType)
-        {
-            case Interactable.SoundType.Player:
-                curdic = PlayerClips;
-                break;
-
-            case Interactable.SoundType.Props:
-                curdic = PropsClips;
-                break;
-
-            case Interactable.SoundType.Enviroment:
-                curdic= EnviromentClips;
-                break;
-
-            case Interactable.SoundType.Default:
-                curdic = IdentiyuClips;
-                break;
-        }
-
-        if (curdic == null)
-        {
-            Debug.LogError("The type does not exist");
-            return null;
-        }
-        else return curdic;
     }
 
     /// <summary>
@@ -146,8 +98,17 @@ public class AudioManager : MonoBehaviour
             case Type.Player:
                 curdic = PlayerClips;
                 break;
+
             case Type.HorrorEvnets:
                 curdic = HorrorEvnetsClips;
+                break;
+
+            case Type.Interactable:
+                curdic = InteractableClips;
+                break;
+
+            case Type.Environment:
+                curdic = EnviromentClips;
                 break;
         }
 

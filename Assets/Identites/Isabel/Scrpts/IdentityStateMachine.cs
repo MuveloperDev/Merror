5c21a1cf6981af839cb6dc9292c2f6a417c62578
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-11-22 17:24:09 +0900

    Update Isabel
    Update Isabel

--- Assets/Identites/Isabel/Scrpts/IdentityStateMachine.cs
+++ Assets/Identites/Isabel/Scrpts/IdentityStateMachine.cs
@@ -10,7 +10,13 @@ public class IdentityStateMachine : BaseStateMachine
 
     public override void TurnOnState(State STATE) => base.TurnOnState(STATE);
     public override void TurnOffState() => base.TurnOffState();
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TurnOnState(State.CHASE);
+        }
+    }
     public void WaitForDisable(float time) => Invoke("Disable", time);
 
     private void Disable() => gameObject.SetActive(false);
